430d07a04da8db0c4b3201c59aac51a9af10c0f6
Rkauff <koontz2k4@gmail.com>
2018-11-23 23:24:35 -0600

    Update Twilio Checking Caller Name Function.py
    Minor change to the number lookup function

--- Twilio Checking Caller Name Function.py
+++ Twilio Checking Caller Name Function.py
@@ -19,6 +19,6 @@ number_to_lookup = "+16307889299"
 
 def number_lookup(number_to_lookup):
     name = client.lookups.phone_numbers(number_to_lookup).fetch(type='caller-name')
-    print(name.caller_name)
+    print((name.caller_name)['caller_name'])
     
 number_lookup(number_to_lookup)
