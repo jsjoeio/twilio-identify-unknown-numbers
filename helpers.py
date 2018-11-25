c19fdd7d23731a55db58da39ee6bdef47eaf5344
Joe Previte <jjprevite@gmail.com>
2018-11-25 15:52:49 -0700

    Move all functions to helpers.py

--- helpers.py
+++ helpers.py
@@ -8,7 +8,18 @@ account_sid = os.getenv("ACCOUNT_SID")
 auth_token = os.getenv("AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 
-def sendMessage(message):
+def lookup_number(number_to_lookup):
+  """
+  This function accepts a number (expected format +1XXXXXXXXXX) and returns the callers name.
+  """
+  name = client.lookups.phone_numbers(number_to_lookup).fetch(type='caller-name')
+  if name:
+    return (name.caller_name)['caller_name']
+  else:
+    return False
+
+
+def send_message(message):
   """
   This function accepts as message (String), and then runs the function to send a text
   using Twilio's SMS API.
@@ -20,7 +31,7 @@ def sendMessage(message):
           to=os.getenv("PHONE_NUMBER")
                   )
 
-def parseNumber(message):
+def parse_number(message):
   """
   This function accepts a message (String), which has a phone number inside.
   If it finds a phone number, it parses it then returns the phone number.
